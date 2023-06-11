1d018792124233dc4190db8089358f454500e083
shehan <shehan.salinda92@gmail.com>
2023-06-11 10:21:07 +0530

    undo jars

---
+++ src/main/java/com/uib/api/dtos/returnDTOs/InputParserSetting/InputParserSettingDTO.java
@@ -0,0 +1,12 @@
+package com.uib.api.dtos.returnDTOs.InputParserSetting;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InputParserSettingDTO {
+    private InputParserSettings inputParserSettings;
+}
